32a2669323e8a3db5821a0fac040d249a8f43cc3
zengxiangyi <zengxiangyi@126.com>
2021-02-17 21:49:38 +0800

    -- 修改逻辑

--- src/main/java/com/zxy/com/zxy/GreetingResource.java
+++ src/main/java/com/zxy/com/zxy/GreetingResource.java
@@ -1,15 +1,16 @@
-package com.zxy;
+package com.zxy.com.zxy;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
+@Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("/info")
     public String hello() {
         return "Hello RESTEasy";
     }
